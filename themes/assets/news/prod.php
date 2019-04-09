827844805d3e7e7e5d35f35bb3a4a0fc46f5e226
Bhagwat Singh Chouhan <bhagwat.chouhan@vulpinecode.com>
2019-04-09 08:57:52 +0530

    Applied quick fixes.

--- themes/assets/news/prod.php
+++ themes/assets/news/prod.php
@@ -2,36 +2,48 @@
 
 return [
 	'yii\web\JqueryAsset' => false,
+	'lazy' => [
+		'class' => 'yii\web\AssetBundle',
+		'basePath' => '@webroot',
+		'baseUrl' => '@web',
+		'js' => [ 'news/lzynlaxd-20190216.js' ]
+	],
+	'fa' => [
+		'class' => 'yii\web\AssetBundle',
+		'basePath' => '@webroot',
+		'baseUrl' => '@web',
+		'css' => [ 'news/fawnlaxd-20190216.js' ]
+	],
 	'common' => [
 		'class' => 'yii\web\AssetBundle',
 		'basePath' => '@webroot',
 		'baseUrl' => '@web',
-		'css' => [ 'blog/cmncmgnl-20181201.css' ],
-		'js' => [ 'blog/cmncmgnl-20181201.js' ],
-		'depends' => [ 'cmsgears\assets\jquery\Jquery' ]
+		'css' => [ 'news/cmnnlaxd-20190216.css' ],
+		'js' => [ 'news/cmnnlaxd-20190216.js' ],
+		'depends' => [ 'lazy', 'fa', 'cmsgears\assets\jquery\Jquery' ]
 	],
 	'landing' => [
 		'class' => 'yii\web\AssetBundle',
 		'basePath' => '@webroot',
 		'baseUrl' => '@web',
-		'css' => [ 'blog/ladcmgnl-20181201.css' ],
-		'js' => [ 'blog/ladcmgnl-20181201.js' ],
+		'css' => [ 'news/ladnlaxd-20190216.css' ],
+		'js' => [ 'news/ladnlaxd-20190216.js' ],
 		'depends' => [ 'common' ]
 	],
 	'public' => [
 		'class' => 'yii\web\AssetBundle',
 		'basePath' => '@webroot',
 		'baseUrl' => '@web',
-		'css' => [ 'blog/pubcmgnl-20181201.css' ],
-		'js' => [ 'blog/pubcmgnl-20181201.js' ],
+		'css' => [ 'news/pubnlaxd-20190216.css' ],
+		'js' => [ 'news/pubnlaxd-20190216.js' ],
 		'depends' => [ 'common' ]
 	],
 	'private' => [
 		'class' => 'yii\web\AssetBundle',
 		'basePath' => '@webroot',
 		'baseUrl' => '@web',
-		'css' => [ 'blog/prvcmgnl-20181201.css' ],
-		'js' => [ 'blog/prvcmgnl-20181201.js' ],
+		'css' => [ 'news/prvnlaxd-20190216.css' ],
+		'js' => [ 'news/prvnlaxd-20190216.js' ],
 		'depends' => [ 'common' ]
 	]
 ];
